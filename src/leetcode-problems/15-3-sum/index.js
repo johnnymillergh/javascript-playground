22825c97ce8584d02f87e9fba4fb702acf7f15ac
Johnny Miller (锺俊) <johnnysviva@outlook.com>
2020-09-01 17:54:00 +0800

    feat(LeetCode): add correct 3 sum solution
    Inspired by LeetCode 3Sum Solution Explained - Java
    https://youtu.be/qJSPYnS35SE

--- src/leetcode-problems/15-3-sum/index.js
+++ src/leetcode-problems/15-3-sum/index.js
@@ -64,3 +64,37 @@ const checkDuplicate = (threeSums, target) => {
   }
   return false
 }
+
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+export const threeSum = (nums) => {
+  nums.sort((a, b) => a - b)
+  const solution = []
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+      let low = i + 1
+      let high = nums.length - 1
+      const sum = 0 - nums[i]
+      while (low < high) {
+        if (nums[low] + nums[high] === sum) {
+          solution.push([nums[i], nums[low], nums[high]])
+          while (low < high && nums[low] === nums[low + 1]) {
+            low++
+          }
+          while (low < high && nums[high] === nums[high - 1]) {
+            high--
+          }
+          low++
+          high--
+        } else if (nums[low] + nums[high] > sum) {
+          high--
+        } else {
+          low++
+        }
+      }
+    }
+  }
+  return solution
+}
